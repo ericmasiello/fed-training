ffd7079b6b3ef41b86bcbb85aa0f80ab075e031b
masielle <masielle@advisory.com>
2014-09-15 17:24:03 -0400

    Remove extra abstraction. jsut return publicAPI

--- part10b/js/account.js
+++ part10b/js/account.js
@@ -2,58 +2,56 @@
 
 var account = (function (){
 
-	var Account = {
-
-		init: function(){
-
-			this.publicAPI = {
-				homeNumber1: ko.observable(),
-				homeNumber2: ko.observable(),
-				homeNumber3: ko.observable(),
-				homeNumberExt: ko.observable(),
-				sameAsCell: ko.observable(false),
-				cellNumber1: ko.observable(),
-				cellNumber2: ko.observable(),
-				cellNumber3: ko.observable(),
-				cellNumberExt: ko.observable(),
-				newPassword: ko.observable(''),
-				newPassword2: ko.observable('')
-			};
-
-			/*
-			 * Automatically check whenever newPassword or newPassword2 changes
-			 */
-			this.publicAPI.isPasswordMismatch = ko.computed(function(){
-
-				var misMatch = false;
-
-				if( this.publicAPI.newPassword() !== '' && this.publicAPI.newPassword2() !== '' && this.publicAPI.newPassword() !== this.publicAPI.newPassword2() ){
-
-					misMatch = true;
-				}
-
-				return misMatch;
-
-			}, this );
-
-			/*
-			 * Sets up subscription to changes on sameAsCell observbale
-			 */
-			this.publicAPI.sameAsCell.subscribe(function(checked){
-
-				if( checked === true ){
-					this.publicAPI.cellNumber1(this.publicAPI.homeNumber1());
-					this.publicAPI.cellNumber2(this.publicAPI.homeNumber2());
-					this.publicAPI.cellNumber3(this.publicAPI.homeNumber3());
-					this.publicAPI.cellNumberExt(this.publicAPI.homeNumberExt());
-				}
-
-				return true; //must return true to allow defualt browser behavior
-			}, this );
-
-			return this.publicAPI;
-		}
+	var publicAPI = {
+		homeNumber1: ko.observable(),
+		homeNumber2: ko.observable(),
+		homeNumber3: ko.observable(),
+		homeNumberExt: ko.observable(),
+		sameAsCell: ko.observable(false),
+		cellNumber1: ko.observable(),
+		cellNumber2: ko.observable(),
+		cellNumber3: ko.observable(),
+		cellNumberExt: ko.observable(),
+		newPassword: ko.observable(''),
+		newPassword2: ko.observable('')
 	};
 
-	return (Object.create(Account).init());
+	//Private method
+	var isPasswordMismatch = function(){
+
+		var misMatch = false;
+
+		if( publicAPI.newPassword() !== '' && publicAPI.newPassword2() !== '' && publicAPI.newPassword() !== publicAPI.newPassword2() ){
+
+			misMatch = true;
+		}
+
+		return misMatch;
+	};
+
+	//Private method
+	var sameAsCellChanged = function(checked){
+
+		if( checked === true ){
+			publicAPI.cellNumber1(publicAPI.homeNumber1());
+			publicAPI.cellNumber2(publicAPI.homeNumber2());
+			publicAPI.cellNumber3(publicAPI.homeNumber3());
+			publicAPI.cellNumberExt(publicAPI.homeNumberExt());
+		}
+
+		return true; //must return true to allow defualt browser behavior
+	};
+
+	/*
+	 * Automatically check whenever newPassword or newPassword2 changes
+	 */
+	publicAPI.isPasswordMismatch = ko.computed( isPasswordMismatch );
+
+	/*
+	 * Sets up subscription to changes on sameAsCell observbale
+	 */
+	publicAPI.sameAsCell.subscribe(sameAsCellChanged);
+
+	return publicAPI;
+
 })();
