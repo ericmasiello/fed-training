c6d16e703a59a10b021bf01a17330f49d4a3e0c7
masielle <masielle@advisory.com>
2014-09-15 10:33:57 -0400

    Fix gitignores - was accidently excluding vendor min files Add copy phone number functionality.

--- part08a/js/account.js
+++ part08a/js/account.js
@@ -9,7 +9,18 @@ function toggleSameAsCell(){
   var checked = $(this).is(':checked');
   if( checked === true ){
 
-  }
+		$('#home-number-1').val( $('#cell-number-1').val()).prop('enable', false);
+		$('#home-number-2').val( $('#cell-number-2').val() ).prop('enable', false);
+		$('#home-number-3').val( $('#cell-number-3').val() ).prop('enable', false);
+		$('#home-number-ext').val( $('#cell-number-ext').val() ).prop('enable', false);
+
+  } else {
+
+		$('#home-number-1').prop('enable', true);
+		$('#home-number-2').prop('enable', true);
+		$('#home-number-3').prop('enable', true);
+		$('#home-number-ext').prop('enable', true);
+	}
 }
 
 function initAccount(){
