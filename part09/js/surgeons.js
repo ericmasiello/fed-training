e0e645c6510fc8dc89eb99b06f61330e496e0c4d
masielle <masielle@advisory.com>
2014-09-15 13:43:36 -0400

    Refactor JS files using modular pattern

---
+++ part09/js/surgeons.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var surgeons = (function(){
+
+	var isInitialized = false;
+
+	var loadSurgeonsDoneCallback = function(resp){
+
+		/*
+		 <tbody>
+		 <tr>
+		 <td><span class="h4">User 3</span></td>
+		 <td class="right">74</td>
+		 <td class="right">
+		 <a href="#">Manage Duplicates <i class="i-move-right"></i></a>
+		 </td>
+		 </tr>
+		 </tbody>
+		 */
+
+		var $surgeonTable = $('#surgeons');
+		var $noResults = $('#no-results');
+		var html = '';
+
+		//Remove all tbodys from table, preserving the thead and tfoot
+		$surgeonTable.find('tbody').remove();
+
+		if( jQuery.isPlainObject( resp ) === true &&
+			jQuery.isArray( resp.data ) === true &&
+			resp.data.length > 0 ) {
+
+			//Draw results by looping through data
+			for( var i = 0; i < resp.data.length; i++ ){
+
+				//Example: Can use $('#surgeons').find('tbody:first').find('tr').data('obj') to grab entire data object
+				html += '<tbody><tr data-obj=\'' + JSON.stringify(resp.data[i]) + '\'><td><span class="h4">' + resp.data[i].name + '</span></td>';
+				html += '<td class="right">' + resp.data[i].caseVolume + '</td>';
+				html += '<td class="right"><a href="#">Manage Duplicates <i class="i-move-right"></i></a></td></tr></tbody>';
+			}
+
+			$surgeonTable.append(html);
+			$noResults.hide();
+
+		} else {
+
+			$noResults.show();
+		}
+	};
+
+	var loadSurgeons = function(){
+
+		$.ajax({
+			'url': 'sampledata.json',
+			'type': 'get'
+		}).done( loadSurgeonsDoneCallback );
+
+		//Alternative ...
+		//$.get('sampledata.json', loadSurgeonsDoneCallback);
+	};
+
+	var init = function(){
+
+		if( isInitialized === false ){
+
+			loadSurgeons();
+		}
+
+		isInitialized = true;
+	};
+
+	return {
+
+		init: init
+	};
+
+})();
