b5f502d47e6fae47e2dc1831e74e01adceb6bb9a
masielle <masielle@advisory.com>
2014-09-15 11:23:53 -0400

    Add missing vendor files Add toggleSameAsCell and isPasswordMatch methods Remove/hide error states on index.html

--- part08a/js/account.js
+++ part08a/js/account.js
@@ -2,6 +2,19 @@
 
 function isPasswordMatch(){
 
+	var $password = $('#new-password');
+	var $password2 = $('#new-password-2');
+	var setError = false;
+
+	if( $password.val() !== '' && $password2.val() !== '' && $password.val() !== $password2.val() ){
+
+		setError = true;
+	}
+
+	$password2.toggleClass('is-error', setError)
+		.siblings('label').toggleClass('is-error', setError)
+		.end()
+		.siblings('div, i').toggle(setError);
 }
 
 function toggleSameAsCell(){
@@ -9,23 +22,24 @@ function toggleSameAsCell(){
   var checked = $(this).is(':checked');
   if( checked === true ){
 
-		$('#home-number-1').val( $('#cell-number-1').val()).prop('enable', false);
-		$('#home-number-2').val( $('#cell-number-2').val() ).prop('enable', false);
-		$('#home-number-3').val( $('#cell-number-3').val() ).prop('enable', false);
-		$('#home-number-ext').val( $('#cell-number-ext').val() ).prop('enable', false);
+		$('#home-number-1').val( $('#cell-number-1').val() ).prop('disabled', checked);
+		$('#home-number-2').val( $('#cell-number-2').val() ).prop('disabled', checked);
+		$('#home-number-3').val( $('#cell-number-3').val() ).prop('disabled', checked);
+		$('#home-number-ext').val( $('#cell-number-ext').val() ).prop('disabled', checked);
 
   } else {
 
-		$('#home-number-1').prop('enable', true);
-		$('#home-number-2').prop('enable', true);
-		$('#home-number-3').prop('enable', true);
-		$('#home-number-ext').prop('enable', true);
+		$('#home-number-1').prop('disabled', checked);
+		$('#home-number-2').prop('disabled', checked);
+		$('#home-number-3').prop('disabled', checked);
+		$('#home-number-ext').prop('disabled', checked);
 	}
 }
 
 function initAccount(){
 
   $('#same-as-cell').on('click', toggleSameAsCell);
+	$('#new-password-2, #new-password').on('change', isPasswordMatch);
 }
 
 $(document).ready(initAccount);
